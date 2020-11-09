cfa7c3c8eac14f7401bf342d605d57d82c387df9
Alexander Kolb <alexander.kolb@ottogroup.com>
2020-11-09 21:14:46 +0100

    first working struct

--- pandemic-rust/src/cities.rs
+++ pandemic-rust/src/cities.rs
@@ -1,9 +1,23 @@
-struct City<'a> {
+#[derive(Debug)]
+pub enum Virus {
+    Blue,
+    Red,
+    Yellow,
+    Black
+}
+
+#[derive(Debug)]
+pub struct City<'a> {
     // The 'a defines a lifetime
     name: &'a str,
-    lat: float,
-    lon: float,
-    color: int,
-    neighbors: Set[int],
-    text_alignment: str = "left",
+    lat: f32,
+    lon: f32,
+    color: Virus,
+    neighbors: &'a[i32],
+    text_alignment: &'a str,
 }
+
+
+pub const ALGIERS: City = City { name: "Algiers", lat: 36.7753606, lon: 3.0601882, color: Virus::Blue, neighbors: &[8, 26, 36, 15], text_alignment: "left" };
+
+
