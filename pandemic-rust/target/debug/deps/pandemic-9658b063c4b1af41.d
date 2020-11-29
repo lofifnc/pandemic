0c009a9ebc858344896a0dfcf27a7314d97d5b3e
Alexander Kolb <alexander.kolb@ottogroup.com>
2020-11-29 18:47:40 +0100

    built base

---
+++ pandemic-rust/target/debug/deps/pandemic-9658b063c4b1af41.d
@@ -0,0 +1,7 @@
+/Users/alex/git/pandemic/pandemic-rust/target/debug/deps/pandemic-9658b063c4b1af41: src/lib.rs src/pandemic.rs src/cities.rs
+
+/Users/alex/git/pandemic/pandemic-rust/target/debug/deps/pandemic-9658b063c4b1af41.d: src/lib.rs src/pandemic.rs src/cities.rs
+
+src/lib.rs:
+src/pandemic.rs:
+src/cities.rs:
